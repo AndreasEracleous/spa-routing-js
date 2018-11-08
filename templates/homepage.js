90625eed59a991e77c0272edb8c8b33f20e77ca7
Andreas <andrew.eracleous@gmail.com>
2018-11-08 18:30:50 +0200

    Addded img folder

--- templates/homepage.js
+++ templates/homepage.js
@@ -1,3 +1,7 @@
-var homePage = `
-	<h1>Title here for HOMEPAGE</h1>
+var data = {name: "Andreas"};
+
+var homePage = `<h1>Title here for HOMEPAGE - ${data.name}</h1>
+	<img src="http://localhost:8888/myproject/github/spa-routing-js/img/BMW-TA.jpg" alt="" class="img-fluid" />
 `;
+
+//homePage.apply.(data);
